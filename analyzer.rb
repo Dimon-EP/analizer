33274350c7d4225375037b5aa666746f8d02cf05
Dima <dmitry.bakanov@easternpeak.com>
2019-08-29 22:48:24 +0300

    - README - small fix

--- analyzer.rb
+++ analyzer.rb
@@ -5,21 +5,36 @@ require 'pathname'
 class Analyzer
 
   def initialize(params)
-    raise ArgumentError.new 'Arguments not found!' if params.empty?
+    if params.length < 2
+      puts 'Arguments not found!'
+      return  
+    end
     element = params[2] || '#make-everything-ok-button'
 
     #Try to find 'element' in the original file
     original_path = Pathname.new(params[0])
+    unless File.file? original_path
+      puts 'Arguments not found!'
+      return
+    end
     original_file = Nokogiri::HTML(open(original_path))
     original = original_file.search(element).first
     # puts "Original file - #{(original && original.path) ? 'Ok': 'Fail'}"
 
     #Try to find 'element' in the sample file
     sample_path = Pathname.new(params[1])
+    unless File.file? sample_path
+      puts 'Sample path not found!'
+      return
+    end
     sample_file = Nokogiri::HTML(open(sample_path))
     sample = sample_file.search(element).first
-    puts sample.path
+    puts sample.path if sample
   end
 end
 
+if ARGV.empty?
+  puts 'Arguments not found!'
+  return
+end
 Analyzer.new(ARGV)
