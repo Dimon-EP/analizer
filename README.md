9a230aa53063188405e41e565b9d98c6c0046411
Dima <dmitry.bakanov@easternpeak.com>
2019-08-29 22:40:38 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# analizer
+## Installation
+
+
+```
+gem install nokogiri
+```
+
+### Usage
+
+```
+ruby analizer.rb <input_origin_file_path> <input_other_sample_file_path> [<css_or_xpath_selector>]
+```
+
+Example #1
+```
+ruby analyzer.rb ./examples/sample-0-origin.html ./examples/sample-1-evil-gemini.html
+```
+
+Example #2
+
+```
+ruby analyzer.rb ./examples/sample-0-origin.html ./examples/sample-2-container-and-clone.html .dropdown-menu
+```
---
+++ analyzer.rb
@@ -0,0 +1,25 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pathname'
+
+class Analyzer
+
+  def initialize(params)
+    raise ArgumentError.new 'Arguments not found!' if params.empty?
+    element = params[2] || '#make-everything-ok-button'
+
+    #Try to find 'element' in the original file
+    original_path = Pathname.new(params[0])
+    original_file = Nokogiri::HTML(open(original_path))
+    original = original_file.search(element).first
+    # puts "Original file - #{(original && original.path) ? 'Ok': 'Fail'}"
+
+    #Try to find 'element' in the sample file
+    sample_path = Pathname.new(params[1])
+    sample_file = Nokogiri::HTML(open(sample_path))
+    sample = sample_file.search(element).first
+    puts sample.path
+  end
+end
+
+Analyzer.new(ARGV)
